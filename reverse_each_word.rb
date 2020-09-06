25bf2733b42462f3636016b7081a2cbbfecdb685
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-06 00:13:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   new_arr = []
   arr = sentence.split(" ")
     arr.each do |word|
-    new_arr << word.reverse(" ")
+    new_arr << word.reverse
   end
-  return new_arr.join
+  return new_arr.join(" ")
 end
