a19a395193802e3e7b31c71cefa2043174e0eb1e
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-06 00:16:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,4 +9,10 @@ end
 
 
 def reverse_each_word(sentence)
-  
+    new_arr = []
+  arr = sentence.split(" ")
+    arr.collect do |word|
+    new_arr << word.reverse
+  end
+  return new_arr.join(" ")
+end
